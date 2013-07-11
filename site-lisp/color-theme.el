aef2c1490140c688ea3e45436b496b3ec1fff534
LI Geng <angus.lee8329@gmail.com>
2013-07-12 07:32:36 +0800

    color-theme

--- site-lisp/color-theme.el
+++ site-lisp/color-theme.el
@@ -234,6 +234,17 @@
 
 ;; That's why we depend on cus-face.el functionality.
 
+(defun plist-to-alist (the-plist)
+  (defun get-tuple-from-plist (the-plist)
+    (when the-plist
+      (cons (car the-plist) (cadr the-plist))))
+
+  (let ((alist '()))
+    (while the-plist
+      (add-to-list 'alist (get-tuple-from-plist the-plist))
+      (setq the-plist (cddr the-plist)))
+  alist))
+
 (cond ((fboundp 'custom-face-attributes-get)
        (defalias 'color-theme-face-attr-construct
 	 'custom-face-attributes-get))
