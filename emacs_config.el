1914d81f99a73b865d2c45a0a3ae1fb34b08e230
Angus LEe <angus.lee8329@gmail.com>
2013-11-19 15:23:01 +0800

    add paredit

--- emacs_config.el
+++ emacs_config.el
@@ -27,6 +27,18 @@
 
 (add-hook 'cider-repl-mode-hook 'subword-mode)
 
+;; paredit
+(unless (package-installed-p 'paredit)
+  (package-install 'paredit))
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
 ;; character encoding auto-detection:
 (load-file "~/.emacs.d/site-lisp/unicad.el")
 (require 'unicad)
@@ -252,6 +264,11 @@
 (slime-setup)
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
 (add-hook 'lisp-mode-hook 'auto-complete-mode)
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit t)
 (define-key lisp-mode-map [(return)] 'newline-and-indent)
 
 ;; emacs-nav
