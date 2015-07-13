ddcb04c3362b7f2667a362e4fec966af2d2ac539
Angus LEe <angus.lee8329@gmail.com>
2015-07-13 19:13:55 +0800

    eim

--- emacs_config.el
+++ emacs_config.el
@@ -201,6 +201,23 @@
 ;; (setq c-default-style '((c++-mode . "pyramid") (c-mode . "pyramid")
 ;; (java-mode . "java") (awk-mode . "awk") (other . "gnu")))
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-eim")
+(autoload 'eim-use-package "eim" "Another emacs input method")
+;; Tooltip 暂时还不好用
+(setq eim-use-tooltip nil)
+
+(register-input-method
+ "eim-wb" "euc-cn" 'eim-use-package
+ "五笔" "汉字五笔输入法" "wb.txt")
+(register-input-method
+ "eim-py" "euc-cn" 'eim-use-package
+ "拼音" "汉字拼音输入法" "py.txt")
+
+;; 用 ; 暂时输入英文
+(require 'eim-extra)
+(global-set-key ";" 'eim-insert-ascii)
+
+
 (setq auto-mode-alist
 	  (append '(("\\.h$" . c++-mode)) auto-mode-alist))
 
