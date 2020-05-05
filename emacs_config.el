f1f919e1b276b17060d68ecbfbd052c27270ecf3
LI Geng <angus.lee8329@gmail.com>
2020-05-05 14:43:38 +0000

    Move to package.el

--- emacs_config.el
+++ emacs_config.el
@@ -6,29 +6,27 @@
 (set-language-environment "UTF-8")
 (add-to-list 'load-path "~/.emacs.d/site-lisp")
 
-
-
 ;; install packages
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" .
-               "https://melpa.org/packages/"))
+	         '("melpa" .
+			   "https://melpa.org/packages/"))
 
 ; (package-refresh-contents)
 (package-initialize)
 (defvar my-packages
-  '(company paredit smartparens rainbow-delimiters auctex scala-mode2 jdee))
+  '(company company-emoji paredit smartparens rainbow-delimiters scala-mode2 jdee xcscope
+            php-mode google-c-style ecb git git-blamed lua-mode
+            markdown-mode markdown-mode+ markdown-preview-eww
+            markdown-toc markdownfmt json-mode restclient auctex))
 (dolist (p my-packages)
   (unless (package-installed-p p)
-    (package-install p)))
+    (ignore-errors
+      (package-install p))))
 
-;; popup
-(add-to-list 'load-path "~/.emacs.d/site-lisp/popup-el")
-
-;; auto-complete-mode:
-;; (add-to-list 'load-path "~/.emacs.d/site-lisp/auto-complete")
-;; (require 'auto-complete-config)
-;; (ac-config-default)
+;; cscope:
+(setq cscope-do-not-update-database t)
+(require 'xcscope)
 
 ;; company
 (add-hook 'after-init-hook 'global-company-mode)
@@ -45,21 +43,6 @@
 ;; see https://www.gnu.org/software/emacs/manual/html_node/elisp/Creating-Frames.html
 (add-hook 'after-make-frame-functions 'darwin-set-emoji-font)
 
-;; predictive-mode
-(add-to-list 'load-path "~/.emacs.d/site-lisp/predictive")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/predictive/misc")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/predictive/texinfo")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/predictive/latex")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/predictive/html")
-(autoload 'predictive-mode "predictive" "predictive" t)
-(set-default 'predictive-auto-add-to-dict t)
-(setq predictive-auto-learn t
-      predictive-add-to-dict-ask nil
-      predictive-use-auto-learn-cache nil
-      predictive-which-dict t)
-(require 'predictive)
-
-
 ;; paredit
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
@@ -72,25 +55,6 @@
 ;; rainbow-delimiters
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
-;; cider
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-(setq nrepl-hide-special-buffers t)
-(setq cider-repl-pop-to-buffer-on-connect t)
-(setq cider-popup-stacktraces nil)
-(setq cider-auto-select-error-buffer t)
-(setq nrepl-buffer-name-show-port t)
-(setq cider-repl-display-in-current-window t)
-(setq cider-repl-wrap-history t)
-
-(add-hook 'cider-repl-mode-hook 'subword-mode)
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
-(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'cider-repl-mode-hook 'auto-complete-mode)
-
-;; character encoding auto-detection:
-(load-file "~/.emacs.d/site-lisp/unicad.el")
-(require 'unicad)
-
 ;; default to better frame titles
 (setq frame-title-format
       (concat  "%b - emacs@" (system-name)))
@@ -101,25 +65,12 @@
 (column-number-mode t)
 (setq default-directory "~/")
 
-;; color themes:
-(load-file "~/.emacs.d/site-lisp/color-theme.el")
-(require 'color-theme)
-(cond (window-system
-       (color-theme-deep-blue)
-))
-
-;; eassist
-(require 'eassist)
-
-
 ;; cscope:
 (setq cscope-do-not-update-database t)
-(add-to-list 'load-path "~/.emacs.d/site-lisp/xcscope")
-(load-file "~/.emacs.d/site-lisp/xcscope/xcscope.el")
 (require 'xcscope)
 
 ;; cedet:
-;; (load-file "~/.emacs.d/site-lisp/cedet-1.1/common/cedet.el")
+(require 'cedet)
 (setq semantic-load-enable-code-helpers t)
 (setq semantic-load-turn-useful-things-on t)
 (setq semanticdb-project-roots  (list
@@ -136,73 +87,7 @@
 ;; (semantic-add-system-include "/usr/include/" 'c++-mode)
 ;; (semantic-add-system-include "/usr/include/" 'c-mode)
 
-
-;; cc-mode
-(require 'cc-mode)
-
-;; php-mode
-(require 'php-mode)
-
-;; google-c-style
-(require 'google-c-style)
-(add-hook 'c-mode-common-hook 'google-set-c-style)
-(add-hook 'c-mode-common-hook 'google-make-newline-indent)
-(add-hook 'c++-mode-common-hook 'google-set-c-style)
-(add-hook 'c++-mode-common-hook 'google-make-newline-indent)
-
-(setq c-default-style '((c++-mode . "Google") (c-mode . "Google")
-(java-mode . "java") (awk-mode . "awk") (other . "gnu")))
-
-
-;; Create my personal c style which follows the coding standard of the pyramid project
-;; (defconst pyramid-c-style
-;; '((c-basic-offset . 4)
-;; (c-comment-only-line-offset . 0)
-;; (c-hanging-braces-alist . ((substatement-open before after)
-;; (brace-list-open)))
-;; (c-offsets-alist
-;; (statement-block-intro . +)
-;; (substatement-open . 0)
-;; (inline-open . 0)
-;; (substatement-label . 0)
-;; ;;(access-label . 0)
-;; (statement-cont . +))
-;; )
-;; "Pyramid C/C++ Programming Style\nThis style is a modification of
-;; stroustrup style. ")
-;; (c-add-style "pyramid" pyramid-c-style)
-;; (setq c-default-style '((c++-mode . "pyramid") (c-mode . "pyramid")
-;; (java-mode . "java") (awk-mode . "awk") (other . "gnu")))
-
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-eim")
-(autoload 'eim-use-package "eim" "Another emacs input method")
-;; Tooltip 暂时还不好用
-(setq eim-use-tooltip nil)
-
-(register-input-method
- "eim-wb" "euc-cn" 'eim-use-package
- "五笔" "汉字五笔输入法" "wb.txt")
-(register-input-method
- "eim-py" "euc-cn" 'eim-use-package
- "拼音" "汉字拼音输入法" "py.txt")
-
-
-(require 'eim-extra)
-(global-set-key [(control \\)]  'eim-insert-ascii)
-
-(setq auto-mode-alist
-	  (append '(("\\.h$" . c++-mode)) auto-mode-alist))
-
-(add-hook 'c-mode-hook 'semantic-default-c-setup)
-(add-hook 'c++-mode-hook 'semantic-default-c-setup)
-
-(define-key c-mode-base-map [(return)] 'newline-and-indent)
-(global-set-key [(f5)] 'gdb)
-(global-set-key [(f7)] 'compile)
-(define-key c-mode-base-map [(control x) (t)] 'eassist-switch-h-cpp) ;override toggle-source()
-;; (define-key c-mode-base-map [(control c) (u)] 'uncomment-region)
-(define-key c-mode-base-map [(meta \`)] 'c-indent-command)
-
+(require 'mode-local)
 (setq-mode-local c-mode
                  semanticdb-find-default-throttle
                  '(project unloaded system recursive))
@@ -215,19 +100,39 @@
 
 (global-set-key [(control return)] 'semantic-ia-complete-symbol)
 
+;; eassist
+(require 'eassist)
+(define-key c-mode-base-map [(control x) (t)] 'eassist-switch-h-cpp) ;override toggle-source()
 
-(autoload 'senator-try-expand-semantic "senator")
+;; cc-mode
+(require 'cc-mode)
+(setq auto-mode-alist
+	  (append '(("\\.h$" . c++-mode)) auto-mode-alist))
 
+(add-hook 'c-mode-hook 'semantic-default-c-setup)
+(add-hook 'c++-mode-hook 'semantic-default-c-setup)
 
-;; ecb
-(add-to-list 'load-path "~/.emacs.d/site-lisp/ecb")
-(setq stack-trace-on-error t)
+(define-key c-mode-base-map [(return)] 'newline-and-indent)
+(global-set-key [(f5)] 'gdb)
+(global-set-key [(f7)] 'compile)
+(define-key c-mode-base-map [(meta \`)] 'c-indent-command)
+
+;; php-mode
+(require 'php-mode)
+
+;; google-c-style
+(require 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+(add-hook 'c++-mode-common-hook 'google-set-c-style)
+(add-hook 'c++-mode-common-hook 'google-make-newline-indent)
+
 (require 'ecb)
 
 (setq ;ecb-use-speedbar-instead-native-tree-buffer 'source
-      ecb-windows-width 0.25
-      ecb-tip-of-the-day nil
- )
+ ecb-windows-width 0.25
+ ecb-tip-of-the-day nil)
+
 ;; ecb Hot-key binding:
 (if (eq system-type 'darwin)
   (progn
@@ -237,113 +142,38 @@
     (global-set-key [(f9)] 'ecb-activate)
     (global-set-key [(f12)] 'ecb-deactivate)))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(current-language-environment "UTF-8")
- '(doc-view-continuous t)
- '(ecb-options-version "2.40")
- '(jdee-server-dir "~/.emacs.d/site-lisp/jdee-server/jars")
- '(package-selected-packages
-   (quote
-    (jdee uuid smartparens sbt-mode s rainbow-delimiters paredit noflet malabar-mode company auctex)))
- '(safe-local-variable-values
-   (quote
-    ((Base . 10)
-     (Package . HUNCHENTOOT)
-     (Syntax . COMMON-LISP)
-     (encoding . utf-8))))
- '(send-mail-function (quote smtpmail-send-it))
- '(smtpmail-smtp-server "mail.corp.qunar.com")
- '(smtpmail-smtp-service 25))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(if (eq system-type 'darwin)
-    (progn
-      (setq exec-path
-          (append exec-path '("/usr/local/texlive/2013/bin/x86_64-darwin" "/usr/local/bin")))
-      (setenv "PATH" (concat "/usr/local/texlive/2013/bin/x86_64-darwin:" "/usr/local/bin:" (getenv "PATH"))))
-  (progn
-    (setq exec-path
-          (append exec-path '("/usr/local/texlive/2013/bin/x86_64-linux" "/usr/local/bin")))
-    (setenv "PATH" (concat "/usr/local/texlive/2013/bin/x86_64-linux:" "/usr/local/bin:" (getenv "PATH")))))
-
-;; shortcut for ediff-buffers
+;; global key bindings
 (global-set-key [(control c) (d) (f)] 'ediff-buffers)
+(global-set-key [(control c) (u) (r)] 'uncomment-region)
+(global-set-key [(control c) (c) (r)] 'comment-or-uncomment-region)
+(global-set-key [(meta p)] 'previous-buffer)
+(global-set-key [(meta n)] 'next-buffer)
 
-;; svn:
-(require 'psvn)
-(if (eq system-type 'darwin)
-  (global-set-key [(control f10)] 'svn-status)  
-  (global-set-key [(f8)] 'svn-status))
 
 ;; git
-(add-to-list 'load-path "~/.emacs.d/site-lisp/git")
 (require 'git)
-(require 'git-blame)
-(if (eq system-type 'darwin)
-  (global-set-key [(control f11)] 'git-status)
-  (global-set-key [(f11)] 'git-status))
-
-;; multi-eshell
-(require 'multi-eshell)
-;; (global-set-key "\C-c .-e" 'multi-eshell)
-
-;; pg
-(require 'pg)
+(require 'git-blamed)
 
 ;; lua-mode
-(add-to-list 'load-path "~/.emacs.d/site-lisp/lua-mode")
 (setq auto-mode-alist (cons '("\\.lua$" . lua-mode) auto-mode-alist))
 (setq lua-indent-level 4)
 ;;(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (require 'lua-mode)
 (add-hook 'lua-mode-hook 'turn-on-font-lock)
 (add-hook 'lua-mode-hook 'auto-complete-mode)
-;;(define-key lua-mode-map [(control c) (u)] 'uncomment-region)
-;;(define-key lua-mode-map [(control c) (c)] 'comment-region)
 
+;; restclient
+(require 'restclient)
+
+;; json-mode
+(require 'json-mode)
 
 ;; Dired custom keymappings
+(require 'dired)
 (define-key dired-mode-map [(control c) (control s)] 'find-name-dired)
 
 ;; Tramp
 (setq tramp-default-method "ssh")
-
-;; clojure mode
-(autoload 'clojure-mode "clojure-mode" "A major mode for Clojure" t)
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-
-;; slime & lisp mode
-(setq inferior-lisp-program "sbcl")
-(add-to-list 'load-path "~/.emacs.d/site-lisp/slime/")
-(require 'slime-autoloads)
-(slime-setup)
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(add-hook 'lisp-mode-hook 'auto-complete-mode)
-(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
-(defun override-slime-repl-bindings-with-paredit ()
-  (define-key slime-repl-mode-map
-    (read-kbd-macro paredit-backward-delete-key) nil))
-(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit t)
-(define-key lisp-mode-map [(return)] 'newline-and-indent)
-
-;; emacs-nav
-(add-to-list 'load-path "~/.emacs.d/site-lisp/emacs-nav")
-(require 'nav)
-
-
-;; restclient
-(add-to-list 'load-path "~/.emacs.d/site-lisp/restclient")
-(require 'restclient)
-
 
 ;; auctex
 (setq TeX-auto-save t)
@@ -351,37 +181,37 @@
 (setq TeX-PDF-mode t)
 (add-hook 'TeX-mode-hook 'predictive-mode)
 
+;; markdown-mode
 (autoload 'markdown-mode "markdown-mode"
    "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-
-;; json-mode
-(add-to-list 'load-path "~/.emacs.d/site-lisp/json-mode-master")
-(require 'json-mode)
-
-;; global key bindings
-(global-set-key [(control c) (u) (r)] 'uncomment-region)
-(global-set-key [(control c) (c) (r)] 'comment-or-uncomment-region)
-
-;; auto-mode-alist for cql
-(setq auto-mode-alist (cons '("\\.cql$" . sql-mode) auto-mode-alist))
+(autoload 'gfm-mode "markdown-mode"
+   "Major mode for editing GitHub Flavored Markdown files" t)
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
 ;; scala mode
-(require 'scala-mode2)
+(require 'scala-mode)
 (setq scala-indent:step 4)
 (define-key scala-mode-map [(return)] 'newline-and-indent)
 (add-hook 'scala-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'scala-mode-hook 'subword-mode)
 
-;; scheme mode
-(require 'xscheme)
-(add-to-list 'auto-mode-alist '("\\.scm$" . scheme-mode))
-
-;; jdee
-(require 'jdee)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (restclient json-mode scala-mode2 xcscope smartparens scala-mode rainbow-delimiters php-mode paredit markdownfmt markdown-toc markdown-preview-mode markdown-preview-eww markdown-mode+ lua-mode jdee google-c-style git-blamed git ecb company-emoji))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 (ignore-errors
   (load-file "~/.emacs.d/platform-settings.el"))
